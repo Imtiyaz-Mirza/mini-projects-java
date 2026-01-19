88bf164cb1b263d98b23acaa89195c118f1e179c
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-01-19 15:53:56 +0530

    day 3 mini project

---
+++ miniprjsection/miniprjday3.java
@@ -0,0 +1,11 @@
+package miniprjsection;
+
+public class miniprjday3 {
+    public static void main(String args[]) {
+        int Task = 1;
+        do {
+            System.out.println("Task " + Task + " Added !");
+            Task++;
+        } while (Task <= 5);
+    }
+}
