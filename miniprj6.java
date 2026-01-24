37491f75296d75a2e56714c07fda072c54cf4252
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-01-24 15:34:20 +0530

    String Manipulation

---
+++ miniprj6.java
@@ -0,0 +1,11 @@
+public class miniprj6 {
+    public static void main(String args[]) {
+        String word = "Java";
+        System.out.println("Learn : " + word);
+        System.out.println("Words length : " + word.length());
+        System.out.println("To Capital : " + word.toUpperCase());
+        System.out.println("To Small : " + word.toLowerCase());
+        System.out.println("The words index : " + word.charAt(3));
+        System.out.println("Exists or not : " + word.contains("java"));
+    }
+}
