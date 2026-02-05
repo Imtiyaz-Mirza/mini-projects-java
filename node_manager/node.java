23161d759d570f8b99f7917da50155eb6d775574
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-02-05 14:29:43 +0530

    Connected file

--- node_manager/node.java
+++ node_manager/node.java
@@ -1,5 +1,17 @@
 package node_manager;
 
 public class node {
+    private String content;
 
+    public node(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String newContent) {
+        this.content = newContent;
+    }
 }
