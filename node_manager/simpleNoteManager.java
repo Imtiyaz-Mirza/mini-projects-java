a0c70fcb8cee948336d10ca962d09b00533b4869
Imtiyaz-mirza <mirzaimtiyaz.999@gmail.com>
2026-02-05 14:31:27 +0530

    simple file updated with incapsulation

--- node_manager/simpleNoteManager.java
+++ node_manager/simpleNoteManager.java
@@ -1,7 +1,54 @@
 package node_manager;
 
-public class simpleNoteManager {
-    public static void main(String args[]) {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class simpleNoteManager {
+    static ArrayList<node> nodes = new ArrayList<>();
+    static Scanner userInput = new Scanner(System.in);
+
+    public static void main(String args[]) {
+        int choice;
+        do {
+            System.out.println("\n 1. Add Notes ");
+            System.out.println("\n 2. Show Notes");
+            System.out.println("\n 3. Edit Notes");
+            System.out.println("\n 4. Exit ");
+            choice = userInput.nextInt();
+            userInput.nextLine();
+            switch (choice) {
+                case 1:
+                    addNote();
+                    break;
+                case 2:
+                    showNotes();
+                    break;
+                case 3:
+                    editNote();
+                    break;
+            }
+        } while (choice != 4);
+    }
+
+    static void addNote() {
+        System.out.print("Enter note : ");
+        String text = userInput.nextLine();
+        nodes.add(new node(text));
+    }
+
+    static void showNotes() {
+        for (int i = 0; i < nodes.size(); i++) {
+            System.out.println((i + 1) + ". " + nodes.get(i).getContent());
+        }
+    }
+
+    static void editNote() {
+        showNotes();
+        System.out.print("Enter number to edit : ");
+        int index = userInput.nextInt() - 1;
+        userInput.nextLine();
+        System.out.print("New note: ");
+        String newText = userInput.nextLine();
+        nodes.get(index).setContent(newText);
     }
 }
